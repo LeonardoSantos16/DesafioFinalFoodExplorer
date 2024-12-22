b1225c452c7b5b7cc65a4cbf41a59ab09baafec2
leonardo.mendes <leodsmf@gmail.com>
2024-12-22 20:10:28 -0300

    ajuste para deploy

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,5 @@
 import axios from "axios";
 
 export const api = axios.create({
-    baseURL: 'http://localhost:3333',
-    withCredentials: true,
+    baseURL: 'https://food-explorer-api-mf2r.onrender.com'
 })
