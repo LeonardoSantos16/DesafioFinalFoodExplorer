bd5cc0e6f33053c7b39d98222034f24a182d3b79
leonardo.mendes <leodsmf@gmail.com>
2024-12-23 20:58:24 -0300

    api with credentials

--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 
 export const api = axios.create({
-    baseURL: 'https://food-explorer-api-mf2r.onrender.com'
-})
+    baseURL: 'https://food-explorer-api-mf2r.onrender.com',
+    withCredentials: true,
+});
